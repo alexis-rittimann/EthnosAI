b438f629a7f0ae3758f7c44170b5025289ccee7f
alexis <arittimann@ethnosoft.com>
2024-03-28 17:05:41 +0100

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -8,5 +9,7 @@ import alpinejs from "@astrojs/alpinejs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), alpinejs()]
+  integrations: [tailwind(), mdx(), sitemap(), alpinejs()],
+  output: 'server',
+  adapter: netlify(),
 });
